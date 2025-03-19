4a19fa83641253572df8253f939d1df4477c0aa4
Oscar Alexis Dávila Reyes <104913466+Daro25@users.noreply.github.com>
2025-03-19 14:19:32 -0600

    p1

--- App_Expo/sneakyChat/app/index.tsx
+++ App_Expo/sneakyChat/app/index.tsx
@@ -4,7 +4,7 @@ import { useSQLiteContext } from 'expo-sqlite';
 import { drizzle } from 'drizzle-orm/expo-sqlite';
 import * as schema from '@/db/schema';
 import { Nota } from "@/db/schema";
-import { View, FlatList } from "react-native";
+import { View, FlatList, TouchableOpacity, Text } from "react-native";
 import NotaView from "../assets/Componentes/Nota";
 
 export default function Index() {
@@ -12,21 +12,34 @@ export default function Index() {
   const db = useSQLiteContext();
   const drizzleDb = drizzle(db, { schema});
   const [Notas, setNotas] = useState<Nota[]>([]);
-
+  const consulta =async ()=>{
+    const result = await drizzleDb.select().from(schema.notas);
+    setNotas(result)
+  }
   useEffect(()=>{
-    const consulta =async ()=>{
-      const result = await drizzleDb.select().from(schema.notas);
-      setNotas(result)
-    }
     consulta()
   });
+  const addNota = ()=>{
+    const crearNota = async()=>{
+      const result = await drizzleDb.insert(schema.notas).values({
+        titulo: '',
+        descripcion: '',
+        idCategoria: 1
+      });
+      consulta;
+      crearNota();
+    };
+  };
   return (
     <View style={useGlobalStyles().container}>
       <FlatList data={Notas} renderItem={({item})=>(
         <NotaView title={item.titulo} context={item.descripcion}/>
       )}
       keyExtractor={item => item.id.toString()}/>
-      
+      <TouchableOpacity style={[useGlobalStyles().btn_normal, [,{width: 'auto', display:'flex', position:'absolute', bottom:7, right: 7}]]}
+        onPress={addNota}>
+          <Text style={[useGlobalStyles().text, useGlobalStyles().negrita, useGlobalStyles().center]}>Añadir Nota</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- App_Expo/sneakyChat/assets/Componentes/Nota.jsx
+++ App_Expo/sneakyChat/assets/Componentes/Nota.jsx
@@ -4,9 +4,27 @@ import { globalStyles } from "../../app/recursos/style";
 
 export default function NotaView(props) {
     return (
-        <View>
-            <Text>{props.title}</Text>
-            <Text>{props.context}</Text>
+        <View style={styles.container}>
+            <Text style={[globalStyles.title]}>{props.title}</Text>
+            <Text style={[styles.text, globalStyles.text]}>{props.context}</Text>
         </View>
     );
 }
+const styles = StyleSheet.create (
+    {
+        container: {
+            height:'auto',
+            position: 'relative',
+            margin:10,
+            width:'80%',
+            minHeight: 70,
+            padding: 10,
+            borderRadius: 9,
+            position: 'relative',
+            backgroundColor: '#9B7EBD',
+        },
+        text: {
+            marginBottom: 20
+        }
+    }
+);
